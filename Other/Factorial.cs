407904afe15b48ec4d6ddfedbfe03c6b6be158f8
Artem Shkolovy <artem.shkolovy@softheme.com>
2014-01-27 17:31:39 +0200

    add Factoril and Fibonacci algorithms

---
+++ Other/Factorial.cs
@@ -0,0 +1,33 @@
+﻿namespace Other
+{
+    //7! = 1·2·3·4·5·6·7 = 5040
+    public static class Factorial
+    {
+        public static int CalculateWithRecursion(int number)
+        {
+            return Calculate(number, number);
+        }
+
+        private static int Calculate(int number, int result)
+        {
+            if (number > 1)
+            {
+                return Calculate(--number, result*number);
+            }
+
+            return result;
+        }
+
+        public static int Calculate(int number)
+        {
+            int result = number;
+
+            while (number > 1)
+            {
+                result = result*--number;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Other/Fibonacci.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Other
+{
+    //0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584 ....
+    public static class Fibonacci
+    {
+        public static int[] Calculate(int numbersCount)
+        {
+            if (numbersCount < 2)
+            {
+                throw new ArgumentException("numbersCount cannot be less than 2");
+            }
+
+            var result = new int[numbersCount];
+
+            result[0] = 0;
+            result[1] = 1;
+
+            for (int i = 1; i < numbersCount - 1; i++)
+            {
+                result[i + 1] = result[i] + result[i - 1];
+            }
+
+            return result;
+        }
+
+        public static int[] CalculateWithRecursion(int numbersCount)
+        {
+            if (numbersCount < 2)
+            {
+                throw new ArgumentException("numbersCount cannot be less than 2");
+            }
+
+            var result = new int[numbersCount];
+
+            result[0] = 0;
+            result[1] = 1;
+
+            CalculateWithRecursion(result, numbersCount, 1);
+
+            return result;
+        }
+
+        private static void CalculateWithRecursion(int[] arr, int numbersCount, int currentNemberIndex)
+        {
+            if (currentNemberIndex < numbersCount - 1)
+            {
+                arr[currentNemberIndex + 1] = arr[currentNemberIndex] + arr[currentNemberIndex - 1];
+                CalculateWithRecursion(arr, numbersCount, currentNemberIndex + 1);
+            }
+        }
+    }
+}
